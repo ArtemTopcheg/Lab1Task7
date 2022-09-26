a090edd686fdd9fff294e5bc644a4ed468ca51f4
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:28:45 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 7   (0,7,7) (0,139,139) (146,222,76)
+
+Console.WriteLine("Введите два числа типа byte");
+byte First = byte.Parse(Console.ReadLine());
+byte Second = byte.Parse(Console.ReadLine());
+Console.WriteLine("Побитовое И чисел:" + (First & Second));
+Console.WriteLine("Побитовое ИЛИ чисел:" + (First | Second));
+Console.WriteLine("Побитовое исключающее ИЛИ чисел:" + (First ^ Second));
